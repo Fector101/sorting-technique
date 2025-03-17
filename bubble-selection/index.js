20a1e7ae3c285d4f391d5a14720ff5cbbf8096ca
Fabian <fabianjoseph063@gmail.com>
2025-03-17 15:44:33 +0100

    Create index.js

---
+++ bubble-selection/index.js
@@ -0,0 +1,19 @@
+function Sorter(numbers){for (let i = 0; i < numbers.length; i++) {
+  let smallest_number = numbers[i]
+  for (let j = i+1; j < numbers.length; j++) {
+    if(numbers[j] < smallest_number){
+      let new_smallest=numbers[j]
+      numbers[j]=smallest_number
+      smallest_number=new_smallest
+      numbers[i]=new_smallest
+      
+    }
+  }
+}
+console.log(numbers)
+}
+Sorter([3, 1, 2])
+Sorter([64, 25, 12, 22, 11])
+Sorter([5, 4, 3, 2, 1])
+Sorter([1,22,1,223, 355,3,33,-46,32,89,5,2,78])
+Sorter([10, 20, 10, 5, 1]);
