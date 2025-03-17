1dd6ebae7388ef6e437b78f98940ba6aa0a3543e
Fabian <fabianjoseph063@gmail.com>
2025-03-17 16:24:34 +0100

    Cleaner

--- bubble-selection/index.js
+++ bubble-selection/index.js
@@ -1,17 +1,17 @@
-function Sorter(numbers){for (let i = 0; i < numbers.length; i++) {
-  let smallest_number = numbers[i]
-  for (let j = i+1; j < numbers.length; j++) {
-    if(numbers[j] < smallest_number){
-      let new_smallest=numbers[j]
-      numbers[j]=smallest_number
-      smallest_number=new_smallest
-      numbers[i]=new_smallest
-      
+function Sorter(numbers){
+  for (let i = 0; i < numbers.length; i++) {
+    let smallest_number = numbers[i]
+    for (let j = i+1; j < numbers.length; j++) {
+      if(numbers[j] < smallest_number){
+        numbers[i]=numbers[j]
+        numbers[j]=smallest_number
+        smallest_number=numbers[i]
     }
   }
 }
 console.log(numbers)
 }
+Sorter([8, 3, 5, 1]);
 Sorter([3, 1, 2])
 Sorter([64, 25, 12, 22, 11])
 Sorter([5, 4, 3, 2, 1])
